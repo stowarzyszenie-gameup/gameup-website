f6c0d986e911af63e1aa80127893f5d20ea1c8bc
Michał Hawełka <m.hawelka@gmail.com>
2022-05-24 12:08:45 +0200

    Basic mobile menu

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <nav className="flex flex-row justify-between p-6">
-      <div>LOGO</div>
-      <ul className="flex flex-row list-none gap-4">
+    <nav className="flex flex-col sm:flex-row sm:justify-between p-6">
+      <div className="flex flex-row justify-between">
+        <div>
+          <Link href="/">
+            <a>
+              <img src="/gameup_logo.webp" alt="GameUP logo" className="w-36" />
+            </a>
+          </Link>
+        </div>
+        <button
+          className="sm:hidden"
+          onClick={() => setShowMenu(!showMenu)}
+          aria-expanded={showMenu}
+          aria-label="Menu mobilne"
+        >
+          H
+        </button>
+      </div>
+      <ul id="desktop-menu" className="hidden sm:flex flex-row list-none gap-4">
+        <li>
+          <Link href="/#about" scroll={false}>
+            <a>O nas</a>
+          </Link>
+        </li>
+        <li>Warsztaty</li>
+        <li>Zespół</li>
+        <li>Kontakt</li>
+      </ul>
+      <ul
+        id="mobile-menu"
+        className={`${
+          showMenu ? "flex" : "hidden"
+        } sm:hidden flex-col list-none gap-4 items-center`}
+      >
         <li>O nas</li>
         <li>Warsztaty</li>
         <li>Zespół</li>
