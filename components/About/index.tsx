4431e35e68084f941ce36e477f425faca8b7b99d
Michał Hawełka <m.hawelka@gmail.com>
2022-05-24 20:08:25 +0200

    Some content

---
+++ components/About/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div id="about">
+      <h3>CZYM SIĘ ZAJMUJEMY?</h3>
+      <h2>PRACUJEMY NAD ROZWOJEM BRANŻY GIER</h2>
+      <ul>
+        <li>
+          <h5>WSPIERAMY</h5>
+          <p>
+            Wspieramy rozwój instytucji oraz podmiotów gospodarczych z
+            wykorzystaniem gier
+          </p>
+        </li>
+        <li>
+          <h5>BADAMY</h5>
+          <p>
+            Prowadzimy badania, konsultacje oraz audyty dla podmiotów branż
+            kreatywnych oraz innych podmiotów gospodarczych wykorzystujących gry
+            jako medium
+          </p>
+        </li>
+        <li>
+          <h5>SZKOLIMY</h5>
+          <p>
+            Organizujemy szkolenia dokształcające dla osób już działających lub
+            pragnących wejść na rynek pracy związanej z produkcją, wydawnictwem
+            oraz promocją gier
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default About;
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="h-60 sm:h-96 bg-hero bg-cover bg-center w-full">
-      {" "}
-      <h1 className="">Stowarzyszenie GameUp</h1>
+    <div className="h-60 sm:h-96 xl:h-[50vh] bg-hero bg-cover bg-center w-full">
+      <h2 className="">Stowarzyszenie GameUp</h2>
+      <h1>{"WANT TO PLAY? IT'S TIME TO UPGRADE"}</h1>
+      <p>
+        Gry komputerowe, mobilne, planszowe, miejskie – nie mają dla nas
+        tajemnic! Wiemy, że są nie tylko narzędziem rozrywki. To także świetne
+        medium dla nauki, rozwoju, reklamy czy komunikacji. Jesteś ciekaw, w
+        jaki sposób gry mogą pomóc Ci w życiu prywatnym lub zawodowym? Chętnie
+        Ci o tym opowiemy!
+      </p>
+      <button>Dowiedz się więcej</button>
     </div>
   );
 };
