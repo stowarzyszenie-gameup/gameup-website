1bf5d8a3764f48e43add47045f67cb42a496f511
Michał Hawełka <m.hawelka@gmail.com>
2022-05-31 06:31:51 +0200

    Share images

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Contact from "../components/Contact";
 import Hero from "../components/Hero";
 import Team from "../components/Team";
 import Workshops from "../components/Workshops";
+import facebookShareImg from "../components/assets/images/facebook_share.jpg";
+import gameUpLogo from "../components/assets/images/logo.jpg";
 
 const Home: NextPage = () => {
   return (
@@ -23,9 +25,15 @@ const Home: NextPage = () => {
           property="og:title"
           content="Stowarzyszenie Badania i Rozwoju Gier GameUp"
         />
-        <meta property="og:description" content="TBD" />
-        <meta property="og:image" content="TBD" />
+        <meta
+          property="og:description"
+          content="Want to play? It's time to UPGRADE!"
+        />
+        <meta property="og:image" content={facebookShareImg.src} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="twitter:card" content="summary" />
+        <meta property="twitter:image" content={gameUpLogo.src} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
